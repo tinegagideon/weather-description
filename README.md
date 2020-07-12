2ab181c4a7a37842e0f94cd66b170fbbce9ccfef
tinega <tinegagideon@gmail.com>
2020-07-12 12:32:31 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#weather-description
+
+This is a console app that can be used to search for a city's current weather description
+
---
+++ weather.py
@@ -0,0 +1,17 @@
+#importing the requests module to allow sending of HTTP requests
+import requests
+
+
+base_url = 'http://api.openweathermap.org/data/2.5/weather?appid=4b84c48893285fb922690f8ae9d5e5b5&q='
+
+print('current weather description')
+city = input('Search for a city..\n')
+#concatinating the base_url and city input to complete the query from user
+url = base_url + city
+
+json_data = requests.get(url).json()
+weather_description = json_data['weather'][0]['description']
+
+print(weather_description)
+
+quit = input('press any key to quit')

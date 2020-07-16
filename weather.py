75de20200f1c9ad3f377f4ed0beb24c637646eeb
tinega <tinegagideon@gmail.com>
2020-07-16 12:52:18 -0700

    :art: improved code format and added iteration

--- weather.py
+++ weather.py
@@ -1,20 +1,26 @@
 #importing the requests module to allow sending of HTTP requests
 import requests
 
-base_url = 'http://api.openweathermap.org/data/2.5/weather?appid=4b84c48893285fb922690f8ae9d5e5b5&q='
+base_url = 'http://api.openweathermap.org/data/2.5/weather?appid=4b84c48893285fb922690f8ae9d5e5b5&units=metric&q='
 
-print('current weather description')
-city = input('Search for a city..\n')
-#concatinating the base_url and city input to complete the query from user
-try:
-    url = base_url + city
+print('Weathex Description')
+while True:
+    city = input('Search for a city..\n')
+    if city == 'q' or 'Q':
+        break
+    #concatinating the base_url and city input to complete the query from user
+    try:
+        url = base_url + city
 
-    json_data = requests.get(url).json()
-    weather_description = json_data['weather'][0]['description']
+        json_data = requests.get(url).json()
+        weather_description = json_data['weather'][0]['description']
+        country = json_data['sys']['country']
+        temp = json_data['main']['temp']
+        print('Country:',country)
+        print('Temperature:',temp)
+        print(weather_description)
+    except:
+        #Display error when city name is invalid or no internet connection is available
+        print('Invalid city name!\nOr\nNo internet connection')
 
-    print(weather_description)
-except:
-    #Display error when city name is invalid or no internet connection is available
-    print('Invalid city name!\nOr\nNo internet connection')
-
-quit = input('press any key to quit')
+quit = input('press any key to close application')
